98fc25d571c43da503d50367f77ff0e368565994
anthony <anthonydgallo@gmail.com>
2017-07-29 15:10:46 -0400

    4.4 More Updates

--- db/migrate/20170729191001_create_orders.rb
+++ db/migrate/20170729191001_create_orders.rb
@@ -2,9 +2,10 @@ class CreateOrders < ActiveRecord::Migration[5.1]
   def change
     create_table :orders do |t|
       t.integer :user_id
-      t. :product_id
+      t.integer :product_id
       t.float :total
     end
     add_index :orders, :user_id
+    add_index :orders, :product_id
   end
 end
