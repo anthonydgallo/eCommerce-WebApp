19231f4708a2c929e105e44c21ef66490543b84a
anthony <anthonydgallo@gmail.com>
2017-08-12 12:11:56 -0400

    Achievement 4 Enhancements

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -6,7 +6,9 @@ class StaticPagesController < ApplicationController
   end
 
   def landing_page
-  	@featured_product = Product.first
-  	@products = Product.all
+  	if Product.first
+  		@featured_product = Product.first
+  		@products = Product.all
+  	end
   end
 end
