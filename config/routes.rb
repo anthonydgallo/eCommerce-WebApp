a34b22065423afd447de8085ff68f081067f2011
anthony <anthonydgallo@gmail.com>
2017-10-08 12:41:23 -0400

    6.13 ActionCable

--- config/routes.rb
+++ config/routes.rb
@@ -28,6 +28,8 @@ Rails.application.routes.draw do
 
 	root 'static_pages#index'
 
+  mount ActionCable.server => '/cable'
+
 	resources :orders, only: [:index, :show, :create, :destroy]
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
